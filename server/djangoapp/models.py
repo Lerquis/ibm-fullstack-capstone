d0678659f2f554678504a8426446062739823d7c
lerquis <estebanrojasquesada@gmail.com>
2024-12-06 23:58:57 -0500

    Fixing populate.py lint problems

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -8,7 +8,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
-    description = modelsP.TextField()
+    description = models.TextField()
 
     def __str__(self):
         return self.name
