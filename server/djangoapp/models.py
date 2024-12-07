d7ce627e64b0f2487a62c64c662ab3043478156f
lerquis <estebanrojasquesada@gmail.com>
2024-12-06 23:26:42 -0500

    Fixing some lint problems

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,12 +1,8 @@
 # Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-
-
 # Create your models here.
-
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -32,10 +28,7 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2024)
-    validators=[
-        MaxValueValidator(2024),
-        MinValueValidator(2015)
-    ]
+    validators=[MaxValueValidator(2024),MinValueValidator(2015)]
 
     def __str__(self):
         return self.name
